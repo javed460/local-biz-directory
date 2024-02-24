a0d93e9abb7ab400ac95b399c3dd0dfc027654ba
Javed Bagwan <javed460@gmail.com>
2024-02-24 07:42:58 +0530

    Updated Index page

--- src/main/java/com/localbizdirectory/controller/IndexPageController.java
+++ src/main/java/com/localbizdirectory/controller/IndexPageController.java
@@ -1,15 +1,23 @@
 package com.localbizdirectory.controller;
 
+import com.localbizdirectory.entity.BusinessCategory;
+import com.localbizdirectory.service.BusinessCategoryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class IndexPageController {
+import java.util.List;
 
-    @GetMapping("/index")
+@Controller
+@RequiredArgsConstructor
+public class IndexPageController {
+    private final BusinessCategoryService businessCategoryService;
+
+    @GetMapping("/")
     public String viewHomePage(Model model) {
-        //model.addAttribute("allCatList", "list");
+        List<BusinessCategory> categories = businessCategoryService.getAllCategories();
+        model.addAttribute("categories", categories);
         return "index";
     }
 }

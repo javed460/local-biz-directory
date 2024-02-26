7541d1af444b72e06f1def05d916c4c73130bc4e
Javed Bagwan <javed460@gmail.com>
2024-02-26 09:02:10 +0530

    Admin skeleton code

---
+++ src/main/java/com/localbizdirectory/controller/admin/AdminController.java
@@ -0,0 +1,9 @@
+package com.localbizdirectory.controller.admin;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AdminController {
+}

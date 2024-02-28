3b24919e42bdb5dd96aaa2bb74a81d37a459af48
Javed Bagwan <javed460@gmail.com>
2024-02-28 07:25:15 +0530

    Updated initDB to have user table

--- src/main/resources/db-script/initDB.sql
+++ src/main/resources/db-script/initDB.sql
@@ -7,3 +7,15 @@ create TABLE lbd.business_category (
     updatedOn timestamp NULL DEFAULT CURRENT_TIMESTAMP ON update CURRENT_TIMESTAMP,
     PRIMARY KEY ( id )
 );
+
+create TABLE lbd.user (
+    id int NOT NULL AUTO_INCREMENT,
+    username varchar(255),
+    password varchar(500),
+    forename varchar(255),
+    surname varchar(255),
+    role varchar(255),
+    createdOn timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updatedOn timestamp NULL DEFAULT CURRENT_TIMESTAMP ON update CURRENT_TIMESTAMP,
+    PRIMARY KEY ( id )
+);

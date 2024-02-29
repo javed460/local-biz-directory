2dda0148a65fcebd0ff7e42c600acd9ab201638b
Javed Bagwan <javed460@gmail.com>
2024-02-29 07:30:32 +0530

    AdminController initial checkin

--- src/main/java/com/localbizdirectory/controller/admin/AdminController.java
+++ src/main/java/com/localbizdirectory/controller/admin/AdminController.java
@@ -1,9 +1,14 @@
 package com.localbizdirectory.controller.admin;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/admin")
 public class AdminController {
+
+
 }

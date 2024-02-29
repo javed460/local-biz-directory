4357b967f57e28e0a2f4ed40a57d26b11a05cdaf
Javed Bagwan <javed460@gmail.com>
2024-02-29 08:42:08 +0530

    Added createUser endpoint

--- src/main/java/com/localbizdirectory/controller/admin/AdminController.java
+++ src/main/java/com/localbizdirectory/controller/admin/AdminController.java
@@ -1,7 +1,12 @@
 package com.localbizdirectory.controller.admin;
 
+import com.localbizdirectory.dto.UserRequestDTO;
+import com.localbizdirectory.entity.User;
+import com.localbizdirectory.service.UserService;
+import com.localbizdirectory.service.UserTransformer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/admin")
 public class AdminController {
+    private final UserService userService;
+    private final UserTransformer userTransformer;
+
+    @PostMapping("/user")
+    public Integer createUser(@RequestBody UserRequestDTO userRequestDTO) {
+        User user = userTransformer.transform(userRequestDTO);
+        return userService.createUser(user);
+    }
 
 
 }

dbb886fa4465f8130c2b0e4f2a639959a18afc92
Javed Bagwan <javed460@gmail.com>
2024-03-04 07:30:19 +0530

    Updated UserService

--- src/main/java/com/localbizdirectory/service/UserService.java
+++ src/main/java/com/localbizdirectory/service/UserService.java
@@ -1,15 +1,17 @@
 package com.localbizdirectory.service;
 
+import com.localbizdirectory.entity.User;
 import com.localbizdirectory.repo.UserRepo;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private final PasswordEncoder passwordEncoder;
     private final UserRepo userRepo;
 
-
+    public Integer createUser(User user) {
+        User savedUser = userRepo.save(user);
+        return savedUser.getId();
+    }
 }

b058fcef89d3f078b144097f01c57b9212596ac3
Javed Bagwan <javed460@gmail.com>
2024-04-07 08:35:20 +0530

    added DTO

---
+++ src/main/java/com/localbizdirectory/dto/BusinessCategoryDTO.java
@@ -0,0 +1,12 @@
+package com.localbizdirectory.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class BusinessCategoryDTO {
+    private String category;
+}
